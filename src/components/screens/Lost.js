7cb82f062b00d9efa54d9b51a7d216825a0d7bc7
Sreetam07 <sreetamsahu111@gmail.com>
2024-02-25 10:42:25 +0530

    Added the basic components for all screens

---
+++ src/components/screens/Lost.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Lost() {
+  return (
+    <View>
+      <Text>Lost</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
