8ffd62581a363308f510ecb2a4294df96b17192d
Sreetam07 <sreetamsahu111@gmail.com>
2024-02-25 10:48:24 +0530

    Implemented stack navigation

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/components/screens/Login';
+import GamePlay from './src/components/screens/GamePlay';
+import Lost from './src/components/screens/Lost';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Game" component={GamePlay} />
+        <Stack.Screen name="Lost" component={Lost} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
